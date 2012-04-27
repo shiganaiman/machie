91e46b9c2cf10e10743515f6f309959b322b9882
Taiki Nakada <shiganaiman@gmail.com>
2012-04-27 15:34:12 +0900

    commit

--- app/app/equations/Retional.scala
+++ app/app/equations/Retional.scala
@@ -1,9 +1,32 @@
 package app.equations
+import scala.math.BigInt
 
-class Retional(n: Int, d: Int){
-  val numer = n
-  val denom = d
+class Retional(n: Int, d: Int) {
+  require(d != 0)
+  val g = gcd(n.abs, d.abs)
+  val numer = n / g
+  val denom = d / g
+  def this(n: Int) = this(n, 1)
   override def toString = {
-	  n + "/" + d
+    denom match {
+      case 1 => numer.toString()
+      case _ => numer + "/" + denom
+    }
   }
+  def add(that: Retional): Retional =
+    new Retional(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom)
+  def +(that: Retional): Retional = add(that)
+
+  def *(that: Retional): Retional =
+    new Retional(
+      numer * that.numer, denom * that.denom)
+
+  
+  private def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
+
 }
+
+implicit def intToRational(x: Int)= new Retional(x)
--- test/app/equations/RetinalSpec.scala
+++ test/app/equations/RetinalSpec.scala
@@ -2,7 +2,7 @@ package app.equations
 
 import org.specs2.mutable._
 
-class RetionalSpec extends SpecificationWithJUnit {
+class RetionalSpec extends Specification {
 
   "A Retional(1, 2)" should {
     val retional = new Retional(1, 2);
@@ -13,13 +13,62 @@ class RetionalSpec extends SpecificationWithJUnit {
       retional.denom must not beNull
     }
     "be 1" in {
-      retional.numer must equalTo (1)
+      retional.numer must be_==(1)
     }
     "be 2" in {
-      retional.denom must equalTo (2)
+      retional.denom must be_==(2)
     }
     "return '1/2'" in {
-      retional.toString() must equalTo("1/2")
+      retional.toString() must be_==("1/2")
     }
   }
-}
+
+  "A Retional(2, 4)" should {
+    "equal '1/2'" in {
+      new Retional(1, 2).toString() must be_==("1/2")
+    }
+  }
+
+  "A Retional(5, 0)" should {
+    "throw Exception" in {
+      new Retional(5, 0) must throwA[IllegalArgumentException]
+    }
+  }
+
+  "1/3 add 1/3" should {
+    "equal 2/3" in {
+      (new Retional(1, 3) add new Retional(1, 3)).toString() must be_==("2/3")
+
+    }
+  }
+  "3/8 add 1/8" should {
+    "equal 1/2" in {
+      (new Retional(3, 8) add new Retional(1, 8)).toString() must be_==("1/2")
+
+    }
+  }
+  "2/3 add 3/4" should {
+    "equal 17/12" in {
+      (new Retional(2, 3) add new Retional(3, 4)).toString() must be_==("17/12")
+
+    }
+  }
+  "2/3 + 3/4" should {
+    "equal 17/12" in {
+      (new Retional(2, 3) + new Retional(3, 4)).toString() must be_==("17/12")
+
+    }
+  }
+  "2/3 * 3/4" should {
+    "equal 1/2" in {
+      (new Retional(2, 3) * new Retional(3, 4)).toString() must be_==("1/2")
+
+    }
+  }
+  "2 * 2/3" should {
+    "equal 4/3" in {
+      implicit def intToRational(x: Int)= new Retional(x)
+      (2 * new Retional(4, 3)).toString() must be_==("4/3")
+    }
+  }
+ }
